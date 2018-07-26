fd35818facc0867a6a1c91a43855aadde73c5432
edthomas93 <et1270@my.bristol.ac.uk>
2018-07-26 16:19:33 +0100

    Enter player names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+feature 'Enter names' do
+  scenario 'submitting names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with: 'The Hulk'
+    fill_in :player_2_name, with: 'Alex Black-Roberts'
+    click_button 'Submit'
+    expect(page).to have_content 'The Hulk vs. Alex Black-Roberts'
   end
 end

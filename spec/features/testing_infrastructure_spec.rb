9070c67f3b05dd7b70b6b025d75cc5fffebb8496
edthomas93 <et1270@my.bristol.ac.uk>
2018-07-26 18:34:30 +0100

    Web Helpers file, DRY (sign in)

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,13 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'The Hulk'
-    fill_in :player_2_name, with: 'Alex Black-Roberts'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'The Hulk vs. Alex Black-Roberts'
+  end
+end
+
+feature 'View hit points' do
+  scenario 'see Player 2 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Alex Black-Roberts: 60HP'
   end
 end

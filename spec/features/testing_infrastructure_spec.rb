ccc3137705ced359b0671ec80f7f6cfdac5e3be1
edthomas93 <et1270@my.bristol.ac.uk>
2018-07-27 11:56:03 +0100

    Adding attack hyperlink

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,6 +8,15 @@ end
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
     sign_in_and_play
+    click_link('Attack')
     expect(page).to have_content 'Alex Black-Roberts: 60HP'
+  end
+end
+
+feature 'Attacking' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_link('Attack')
+    expect(page).to have_content 'The Hulk attacked Alex Black-Roberts'
   end
 end
